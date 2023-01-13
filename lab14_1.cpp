0b9493cffaca33d8ab6b307b157173c181a2cba5
cherishednoel <panida.stp@icloud.com>
2023-01-13 15:35:40 +0700

    lab14_1.cpp completed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,29 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]) {
+    double sum = 0;
+    double sum2 = 0;
+    double sum3 = 0;
+    double sum4 = 0;
+    double max = A[0];
+    double min = A[0];
+    for(int i = 0; i < N ;i++) {
+        sum += A[i];
+        sum2 += pow(A[i],2);
+        sum3 += log(A[i]);
+        sum4 += 1 / A[i];
+        if(A[i] > max) {
+            max = A[i];
+        } if(A[i] < min) {
+            min = A[i];
+        }
+    }
+    B[0] = sum / N;
+    B[1] = sqrt((sum2 / N) - pow(B[0],2));
+    B[2] = exp(sum3 / N);
+    B[3] = N / sum4;
+    B[4] = max;
+    B[5] = min;
+}
